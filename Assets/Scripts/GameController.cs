c2cb7b2cad31b95c09c97763067288cdd7dbd74f
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2019-01-25 22:57:29 +0100

    Added Game Controller

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public List<GameObject> players = new List<GameObject>();
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
