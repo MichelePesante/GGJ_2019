e508081cf4112a1da02a49f6c18253419094f208
giacomo_pignoni <giacomo.pignoni@gmail.com>
2019-01-25 22:07:26 +0100

    Creazione progetto

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MovementSpeed;
+
+    private Vector3 normalMovement;
+    private Vector3 invertedMovement;
+    private Vector3 flippedMovement;
+    private Vector3 invertedFlippedMovement;
+
+    [Header("Perks Bool")]
+    public bool IsFreezed;
+    public bool IsInverted;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        MovePlayer();
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            IsFreezed = !IsFreezed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            IsInverted = !IsInverted;
+        }
+    }
+
+    public void MovePlayer()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
+        
+        if (!IsFreezed && !IsInverted)
+        {
+            transform.position += SetNormalMovement(x, z);
+        }
+
+        else if (IsInverted)
+        {
+
+        }
+    }
+
+    private Vector3 SetNormalMovement(float x, float z)
+    {
+        return normalMovement = new Vector3(x * MovementSpeed * Time.deltaTime, 0f, z * MovementSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Timer
+{
+    private float timer;
+
+    public float GetTimer()
+    {
+        return timer;
+    }
+
+    public void TickTimer()
+    {
+        timer += Time.deltaTime;
+    }
+
+    public void StopTimer()
+    {
+        timer = 0f;
+    }
+
+    public void PauseTimer()
+    {
+        timer += 0f;
+    }
+
+    public bool CheckTimer(float timerToCheck)
+    {
+        if(timer >= timerToCheck)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
