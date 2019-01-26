05fb1887f6fd0c978aa829194421b55e02884e8f
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2019-01-26 07:01:50 +0100

    Added Throne and ThroneManager

---
+++ Assets/Scripts/Throne.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Throne : MonoBehaviour {
+	private Light throneLight;
+
+	public bool isActive = false;
+
+	private void Awake() {
+		throneLight = GetComponentInChildren<Light>();
+	}
+
+	public void Activate() {
+		isActive = true;
+
+		throneLight.enabled = true;
+	}
+
+	public void Deactivate() {
+		isActive = false;
+
+		throneLight.enabled = false;
+	}
+}
---
+++ Assets/Scripts/ThroneManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class ThroneManager : MonoBehaviour {
+	private List<Throne> thrones = new List<Throne>();
+
+	private void Awake() {
+		thrones = FindObjectsOfType<Throne>().ToList();
+	}
+
+	public void ActiveRandomThrone() {
+		Random rnd = new Random();
+		Throne rndThrone;
+		do {
+			rndThrone = thrones[rnd.Next(0, thrones.Count)];
+		} while(rndThrone.isActive == true);
+
+		rndThrone.Activate();
+		DeactivateThrones(rndThrone);
+	}
+
+	private void DeactivateThrones(Throne toNotDisable) {
+		foreach (Throne throne in thrones)
+		{
+			if(throne != toNotDisable) {
+				throne.Deactivate();
+			}
+		}
+	}
+}
