cf8076850351fd58a17a917e77eaaee1e47bd922
giacomo_pignoni <giacomo.pignoni@gmail.com>
2019-01-26 18:27:12 +0100

    Created ThronePerk

---
+++ Assets/Scripts/Perks/ThronePerk.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThronePerk : Perk
+{
+    public override void TriggerPerk(PlayerController perkOwner)
+    {
+        FindObjectOfType<ThroneManager>().ActiveRandomThrone();
+    }
+}
