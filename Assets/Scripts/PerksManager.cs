ba9299c518695c49c8a4ec6a35d450d03aed9b4a
Giacomo Pignoni <giacomo.pignoni@gmail.com>
2019-01-26 03:13:38 +0100

    Added PerkManager

---
+++ Assets/Scripts/PerksManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Random = System.Random;
+
+public class PerksManager : MonoBehaviour {
+	public List<GameObject> perksPrefab = new List<GameObject>();
+
+	private List<GameObject> perksSpawnPoint = new List<GameObject>();
+
+	private void Awake() {
+		perksSpawnPoint = GameObject.FindGameObjectsWithTag("PerkSpawnPoint").ToList();
+	}
+
+	private void Start() {
+		SpawnPerks();
+	}
+
+	public void SpawnPerks() {
+		Random rnd = new Random();
+		foreach (GameObject perkSpawnPoint in perksSpawnPoint)
+		{
+			Instantiate(perksPrefab[rnd.Next(0, perksPrefab.Count)], perkSpawnPoint.transform.position, Quaternion.identity);
+		}
+	}
+}
